4073a7f5c91ce69f48e73f3d0b344caa9c2679c8
loki <lokeshyadav1098@gmail.com>
2025-04-18 02:45:54 +0530

    Implement token sale functionality with cost calculation and deposit handling

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -3,6 +3,9 @@ pragma solidity >0.8.0 <0.9.0;
 import {Token} from "./Token.sol";
 
 contract Factory {
+    uint256 public constant TARGET = 3 ether;
+    uint256 public constant TOKEN_LIMIT = 500000 ether;
+
     uint256 public immutable platformFee;
     uint256 public totalTokens;
 
@@ -20,6 +23,7 @@ contract Factory {
     }
 
     event Created(address indexed token);
+    event Buy(address indexed token, uint256 amount);
 
     constructor(uint256 _platformFee) {
         platformFee = _platformFee;
@@ -30,6 +34,15 @@ contract Factory {
         uint256 _index
     ) public view returns (TokenSale memory) {
         return tokenToSale[tokens[_index]];
+    }
+
+    function getCost(uint256 _sold) public pure returns (uint256) {
+        uint256 floor = 0.0001 ether;
+        uint256 step = 0.0001 ether;
+        uint256 increment = 10000 ether;
+
+        uint256 cost = (step * (_sold / increment) + floor);
+        return cost;
     }
 
     function create(
@@ -63,6 +76,58 @@ contract Factory {
     }
 
     function buy(address _token, uint256 _amount) external payable {
+        TokenSale storage sale = tokenToSale[_token];
+
+        // check the consitions
+        require(sale.isOpen == true, "Factory: Buying closed");
+        require(_amount >= 1 ether, "Factory: Amount too low");
+        require(_amount <= 10000 ether, "Factory: Amount exceeded");
+
+        // calculate the price of 1 token based upon total bought
+        uint256 cost = getCost(sale.sold);
+        uint256 price = cost * (_amount / 10 ** 18);
+
+        // make sure coorect ETH is sent
+        require(msg.value >= price, "Factory : Insufficient eth recieved");
+
+        // update the sale
+        sale.sold += _amount;
+        sale.raised += price;
+
+        // make sure fund raisin goal isnt met
+        if (sale.sold >= TOKEN_LIMIT || sale.raised >= TARGET) {
+            sale.isOpen = false;
+        }
+
         Token(_token).transfer(msg.sender, _amount);
+
+        // emit an event
+        emit Buy(_token, _amount);
+    }
+
+    function deposit(address _token) external {
+        //    the remaining token balance and the ETH raised
+        //    would go into a liquidity pool like uniswap v3
+        //    for simplicity we will just transfer remaining
+        //    tokens and ETH raised to the creator
+
+        Token token = Token(_token);
+        TokenSale memory sale = tokenToSale[_token];
+
+        require(sale.isOpen == false , "Factory: Target not reached");
+
+        // transfer tokens
+        token.transfer(sale.creator, token.balanceOf(address(this)));
+
+        // transfer ETH raised 
+        (bool success, ) = payable(sale.creator).call{value : sale.raised}("");
+        require(success, "Factory: ETH transfer failed");
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == owner, "Factory: not allowed" );
+
+        (bool success, ) = payable(owner).call{value : _amount}("");
+        require(success, "Factory, ETH transfer to owner failed");
     }
 }
--- test/Factory.js
+++ test/Factory.js
@@ -24,16 +24,18 @@ describe("Factory", function () {
     return { factory, deployer, creator, token, buyer };
   }
 
-  async function buyTokenFixture(){
-    const {factory, creator, token, buyer} = await deployFactoryFixture();
+  async function buyTokenFixture() {
+    const { factory, creator, token, buyer } = await deployFactoryFixture();
 
     const AMOUNT = ethers.parseUnits("10000", 18);
     const COST = ethers.parseUnits("1", 18);
 
-    const transaction = await factory.connect(buyer).buy(await token.getAddress(), AMOUNT, {value : COST})
-    await transaction.wait()
+    const transaction = await factory
+      .connect(buyer)
+      .buy(await token.getAddress(), AMOUNT, { value: COST });
+    await transaction.wait();
 
-    return {factory, token, creator, buyer}
+    return { factory, token, creator, buyer };
   }
 
   describe("Deployment", function () {
@@ -52,7 +54,7 @@ describe("Factory", function () {
     });
   });
 
-  describe("create", () => {
+  describe("Create", () => {
     it("should set the owner", async function () {
       const { factory, token } = await loadFixture(deployFactoryFixture);
       expect(await token.owner()).to.equal(await factory.getAddress());
@@ -65,37 +67,117 @@ describe("Factory", function () {
 
     it("should set the supply", async function () {
       const { token, factory } = await loadFixture(deployFactoryFixture);
-      const totalSupply = ethers.parseUnits("1000000", 18)
-      expect(await token.balanceOf(await factory.getAddress())).to.equal(totalSupply);
+      const totalSupply = ethers.parseUnits("1000000", 18);
+      expect(await token.balanceOf(await factory.getAddress())).to.equal(
+        totalSupply
+      );
     });
 
     it("should update ETH balance", async function () {
       const { factory } = await loadFixture(deployFactoryFixture);
-      const balance = await ethers.provider.getBalance(await factory.getAddress())
+      const balance = await ethers.provider.getBalance(
+        await factory.getAddress()
+      );
       expect(balance).to.equal(FEE);
     });
 
-    it("should create the sale", async function(){
-      const { factory, token, creator } = await loadFixture(deployFactoryFixture);
-      const count = await factory.totalTokens()
-      expect(count).to.equal(1)
+    it("should create the sale", async function () {
+      const { factory, token, creator } = await loadFixture(
+        deployFactoryFixture
+      );
+      const count = await factory.totalTokens();
+      expect(count).to.equal(1);
 
       const sale = await factory.getTokenSale(0);
-      
+
       expect(sale.token).to.equal(await token.getAddress());
       expect(sale.creator).to.equal(creator.address);
       expect(sale.sold).to.equal(0);
       expect(sale.raised).to.equal(0);
       expect(sale.isOpen).to.equal(true);
-    })
-
-
+    });
   });
 
-  describe("buying", ()=>{
-    // contract recieved eth 
-    it("should ")
+  describe("Buying", () => {
+    const AMOUNT = ethers.parseUnits("10000", 18);
+    const COST = ethers.parseUnits("1", 18);
+
+    // contract recieved eth
+    it("should update ETH balance", async function () {
+      const { factory } = await loadFixture(buyTokenFixture);
+      const balance = await ethers.provider.getBalance(
+        await factory.getAddress()
+      );
+      expect(balance).to.equal(COST + FEE);
+    });
 
     //? check that buyer recieved tokens
+    it("should recieve token", async function () {
+      const { buyer, token } = await loadFixture(buyTokenFixture);
+
+      const balance = await token.balanceOf(buyer.address);
+
+      expect(balance).to.equal(AMOUNT);
+    });
+
+    it("should update the token sale", async function () {
+      const { factory, token } = await loadFixture(buyTokenFixture);
+
+      const sale = await factory.tokenToSale(await token.getAddress());
+
+      expect(sale.sold).to.equal(AMOUNT);
+      expect(sale.raised).to.equal(COST);
+      expect(sale.isOpen).to.equal(true);
+    });
+
+    it("should increase the base cost", async function () {
+      const { factory, token } = await loadFixture(buyTokenFixture);
+
+      const sale = await factory.tokenToSale(await token.getAddress());
+      const cost = await factory.getCost(sale.sold);
+
+      expect(cost).to.equal(ethers.parseUnits("0.0002", 18));
+    });
+  });
+
+  describe("Depositing", () => {
+    const AMOUNT = ethers.parseUnits("10000", 18);
+    const COST = ethers.parseUnits("2", 18);
+
+    it("sale should be closed and successfully deposits", async function () {
+      const { factory, buyer, token, creator } = await loadFixture(
+        buyTokenFixture
+      );
+
+      // buy tokens again to reach the target
+      const BuyTx = await factory
+        .connect(buyer)
+        .buy(await token.getAddress(), AMOUNT, { value: COST });
+      await BuyTx.wait();
+
+      const sale = await factory.tokenToSale(await token.getAddress());
+      expect(sale.isOpen).to.equal(false);
+
+      const depositTx = await factory
+        .connect(creator)
+        .deposit(await token.getAddress());
+      await depositTx.wait();
+
+      //! TODO write test for ETH transferred to creator
+    });
+  });
+
+  describe("withdrawing fees", ()=>{
+    it("shouldupdate ETH balances", async function(){
+      const { factory, deployer } = await loadFixture(
+        deployFactoryFixture
+      );
+
+      const transaction = await factory.connect(deployer).withdraw(FEE)
+      await transaction.wait()
+
+      const balance = await ethers.provider.getBalance(await factory.getAddress());
+      expect(balance).to.equal(0)
+    })
   })
 });
